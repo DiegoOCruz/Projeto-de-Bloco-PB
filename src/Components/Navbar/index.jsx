fbb23c787d7c051618232192db0ce3fd8d821079
Diego Oliveira da Cruz <diego.cruz@al.infnet.edu.br>
2024-07-25 20:32:38 -0300

    feat: Update image source paths, add LogoAcme component, and improve CotacoesList page

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,53 +1,50 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import {Link } from "react-router-dom";
-import { useState } from 'react';
-import {LogoAcme as Logo} from '../../Components';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { LogoAcme as Logo } from "../../Components";
 
-
-function ResponsiveAppBar({setLogar, admin}) {
+function ResponsiveAppBar({ setLogar, admin }) {
   React.useEffect(() => {
-    if(admin){
+    if (admin) {
       setPages([
-        { name: 'Home', path: '/' },
-        { name: 'Cotações', path: '/cotacoes' },
-        { name: 'Fornecedores', path: '/fornecedores' },
-        { name: 'Contatos', path: '/contato' },
-        { name: 'Produtos', path: '/produtos' },
-        { name: 'Adicionar Usuários', path: '/register' },
-        { name: 'Requisições', path: '/requisicoes' },
+        { name: "Home", path: "/" },
+        { name: "Cotações", path: "/cotacoes" },
+        { name: "Fornecedores", path: "/fornecedores" },
+        { name: "Contatos", path: "/contato" },
+        { name: "Produtos", path: "/produtos" },
+        { name: "Adicionar Usuários", path: "/register" },
+        { name: "Requisições", path: "/requisicoes" },
       ]);
-    }else{
+    } else {
       setPages([
-        { name: 'Home', path: '/' },
-        { name: 'Cotações', path: '/cotacoes' },
-        { name: 'Fornecedores', path: '/fornecedores' },
-        { name: 'Contatos', path: '/contato' },
-        { name: 'Produtos', path: '/produtos' },
-        { name: 'Adicionar Usuários', path: '/register' },
+        { name: "Home", path: "/" },
+        { name: "Cotações", path: "/cotacoes" },
+        { name: "Fornecedores", path: "/fornecedores" },
+        { name: "Contatos", path: "/contato" },
+        { name: "Produtos", path: "/produtos" },
+        { name: "Adicionar Usuários", path: "/register" },
       ]);
     }
   }, [admin]);
 
-
-  const settings = [ 'Logout'];
+  const settings = ["Logout"];
 
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
   const [pages, setPages] = useState([]);
-  
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -64,12 +61,12 @@ function ResponsiveAppBar({setLogar, admin}) {
     setAnchorElUser(null);
   };
 
-function logout(){
-  setLogar("");
-}
+  function logout() {
+    setLogar("");
+  }
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" color="error">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           {/*<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />*/}
@@ -78,21 +75,21 @@ function logout(){
             variant="h6"
             noWrap
             component={Link}
-            to={'/'}
+            to={"/"}
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             ACME
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -107,26 +104,26 @@ function logout(){
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
-                <MenuItem 
-                key={page.name} 
-                onClick={handleCloseNavMenu}
-                component={Link}
-                to={page.path}
+                <MenuItem
+                  key={page.name}
+                  onClick={handleCloseNavMenu}
+                  component={Link}
+                  to={page.path}
                 >
                   <Typography textAlign="center">{page.name}</Typography>
                 </MenuItem>
@@ -134,7 +131,7 @@ function logout(){
             </Menu>
           </Box>
           {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />*/}
-      
+
           <Typography
             variant="h5"
             noWrap
@@ -142,25 +139,25 @@ function logout(){
             href="#app-bar-with-responsive-menu"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             ACME
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page.name}
                 onClick={handleCloseNavMenu}
                 component={Link}
                 to={page.path}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 {page.name}
               </Button>
@@ -170,28 +167,33 @@ function logout(){
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Logout">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL3Hl5FXWpEFo9p7GTrCTz2jKeSpMPXQJ7JQ&s" />
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL3Hl5FXWpEFo9p7GTrCTz2jKeSpMPXQJ7JQ&s"
+                />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center" onClick={logout}>{setting}</Typography>
+                  <Typography textAlign="center" onClick={logout}>
+                    {setting}
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
